5286ef1f37d47c22f78e2aad1d0d0552f3af3e41
worzhik <gurriericristian@hotmail.com>
2022-07-01 23:45:38 -0300

    Trabajamos en el home, formulario y retrospectiva de scrum

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static('public'));
 
+//app.listen(3000, ()=>{
+//    console.log('Servidor funcionando');
+//});
+
 app.listen(3000, (req, res)=>{
     console.log("Servidor funcionando en http://localhost:3000")
 })
@@ -13,4 +17,8 @@ app.get("/", (req, res)=>{
 
 app.get("/login", (req, res)=>{
     res.sendFile(__dirname + "/views/login.html")
+})
+
+app.get("/formulario", (req, res)=>{
+    res.sendFile(__dirname + "/views/formulario.html")
 })
